b77878f8fcb0df349e43ca2e4dee3b998aba7197
Alexandersaur <jeremy.a.steward@gmail.com>
2021-04-20 20:53:49 +0300

    Logoff functionality

--- FinancialPortal/Controllers/AccountController.cs
+++ FinancialPortal/Controllers/AccountController.cs
@@ -498,7 +498,7 @@ namespace FinancialPortal.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Login", "Account");
         }
 
         //
